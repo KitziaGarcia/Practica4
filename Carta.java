d14c68a381e07d077fd8f03c72ade6659c837c63
KitziaGarcia <kitzia.daniela3101@gmail.com>
2024-10-14 21:08:50 -0700

    reuploading missing classes

---
+++ Carta.java
@@ -0,0 +1,106 @@
+import javax.swing.*;
+
+public class Carta {
+    private int valor;
+    private String figura;
+    private String[] figuras;
+    private ImageIcon imagen;
+
+
+    public Carta() {
+        valor = 1;
+        figura = "oros";
+        //this.imagen = imagen;
+    }
+
+    //ImageIcon imagen
+    public Carta(int valor, String figura, ImageIcon imagen) {
+        this.valor = valor;
+        this.figura = figura;
+        this.imagen = imagen;
+    }
+
+    /**
+     * Método para obtener el valor de la carta.
+     *
+     * @return el valor de la carta.
+     */
+    public int getValor()
+    {
+        return valor;
+    }
+
+    public ImageIcon getImagen() {
+        return imagen;
+    }
+
+    /**
+     * Método para obtener el valor de la letra de la carta.
+     *
+     * @return la letra de la carta.
+     */
+    /*public String getLetra()
+    {
+        return representarLetra(valor);
+    }*/
+
+    /**
+     * Método para obtener la figura de la carta.
+     *
+     * @return la figura de la carta.
+     */
+    public String getFigura()
+    {
+        return figura;
+    }
+
+    /**
+     * Método para definir el valor de la carta.
+     *
+     * @param valor el valor actual de la carta.
+     */
+    public void setValor(int valor)
+    {
+        this.valor = valor;
+    }
+
+    /**
+     * Método para definir la figura de la carta.
+     *
+     * @param figura figura actual de la carta.
+     */
+    public void setFigura(String figura)
+    {
+        this.figura = figura;
+    }
+
+    public String toString(){
+        String cadena;
+        switch (valor)
+        {
+            case 0:
+                cadena = "Joker";
+            case 1:
+                cadena = "A";
+                break;
+            case 10:
+                cadena = "J";
+                break;
+            case 11:
+                cadena = "Q";
+                break;
+            case 12:
+                cadena = "K";
+                break;
+            default:
+                cadena = Integer.toString(valor);
+        }
+        return cadena + " de " + figura;
+    }
+
+    public void printInConsole()
+    {
+        System.out.println(toString());
+    }
+
+}
--- Main.java
+++ Main.java
@@ -5,10 +5,13 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int cantidadDeJugadores;
+        UNO juego = new UNO(3);
 
-        cantidadDeJugadores = Utilidades.isInputValid(input, "Ingrese cantidad de jugadores: ", 2, 4);
+        //cantidadDeJugadores = Utilidades.isInputValid(input, "Ingrese cantidad de jugadores: ", 2, 4);
 
-        UNO juego = new UNO(cantidadDeJugadores);
+        //UNO juego = new UNO(cantidadDeJugadores);
+        //SetJuego juego = new SetJuego();
+
         juego.jugar();
     }
 }
